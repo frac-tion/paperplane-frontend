4a8fe8ecc97b5e3295dcb10fbf1d372ce0b017d8
Tobias Bernard <tobias@zenbook>
2017-01-01 23:35:28 +0100

    [feat] discover peers and render list

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+var peers = {devices: undefined};
+Dropzone.autoDiscover = false;
+
+$( document ).ready(function() {
+  console.log("getting peers");
+  $.get( "http://192.168.1.124:3000/peers", function( data ) {
+    peers.devices = data;
+    for (var i = 0; i < peers.devices.length; i++) {
+      var dev = $('<div id="' + peers.devices[i].name + '" class="device dropzone">' +
+      '<h3>' + peers.devices[i].name + '</h3>' +
+      '<p>' + peers.devices[i].ip  + '</p>' +
+      '</div>');
+      dev.dropzone({ url: 'http://' + peers.devices[i].ip + ':' + peers.devices[i].port + '/upload' });
+      $("#device-list").append(dev);
+    }
+  });
+});
+
+// Dropzone.options[id] = {
+//   paramName: 'file',
+//   maxFilesize: 20, // MB
+//   maxFiles: 1,
+//   dictDefaultMessage: 'Drag an image here to upload, or click to select one',
+//   headers: {
+//     'x-csrf-token': document.querySelectorAll('meta[name=csrf-token]')[0].getAttributeNode('content').value,
+//   },
+//   acceptedFiles: '',
+//   init: function() {
+//     this.on('success', function( file, resp ){
+//       console.log( file );
+//       console.log( resp );
+//     });
+//     this.on('thumbnail', function(file) {
+//       if ( file.width < 640 || file.height < 480 ) {
+//         file.rejectDimensions();
+//       } else {
+//         file.acceptDimensions();
+//       }
+//     });
+//   },
+//   accept: function(file, done) {
+//     file.acceptDimensions = done;
+//     file.rejectDimensions = function() {
+//       done('The image must be at least 640 x 480px');
+//     };
+//   }
+// };
+
+
 // $(".device").on("dragover", function(e) {
 //   e.preventDefault();
 //   console.log("over");
@@ -14,4 +63,4 @@
 //   console.log("drop");
 // });
 
-$("#librem").dropzone({ url: "192.168.1.124:3000/upload" });
+// $("#desktop").dropzone({ url: "http://192.168.1.124:3000/upload" });
